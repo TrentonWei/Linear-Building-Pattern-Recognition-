1bfd0b9ef2e2f5f99e3b7041200b7c53605158f1
Trenton <1098806651@qq.com>
2021-08-15 15:25:59 +0800

    修改注释

--- PrDispalce/FlowMap/FlowMap.cs
+++ PrDispalce/FlowMap/FlowMap.cs
@@ -48,7 +48,7 @@ namespace PrDispalce.FlowMap
         #endregion
 
         /// <summary>
-        /// 深拷贝
+        /// 深拷贝（通用）
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
